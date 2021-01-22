eefe1a58d46f9e3d52e5a6543f57fa8a07d56a56
Mikhail Astapchyk <74508872+astapm@users.noreply.github.com>
2021-01-22 19:57:55 +0300

    Update dices.py

--- dices/dices.py
+++ dices/dices.py
@@ -1,12 +1,14 @@
 #! /usr/bin/env python
 
-# Game "Dices"
+# Игра "Игральные кубики" для терминала
+# Можно выбрать от 1 до 12 шестигранных кубиков
+# После броска выводится очки на кубиках и общая сумма очков
 
 from random import randint
 
-# Вводим количество кубиков
-# но не более 25. По умолчвнию 1 кубик
-num_dices = input("Введиите количество кубиков(макс. 25): ")
+# Вводим количество кубиков, но не более 12.
+# По умолчвнию 1 кубик.
+num_dices = input("Введиите количество кубиков(макс. 12): ")
 if num_dices.isdigit():
     num_dices = int(num_dices)
     if (num_dices > 25):
@@ -19,14 +21,13 @@ print("Выбрано кубиков: ",num_dices,"\n")
 # Бросаем кубики
 choice = ""
 while choice != "q":
-    choice = input("Бросаем кубики? <любая клавиша>, <q> - выход ")
+    choice = input("Бросаем кубики <Ввод>, выход - <q> ")
     if choice != "q":
         sum_point = 0 # сумма очков всех кубиков
         for x in range(num_dices):
             point = randint(1,6)
-            print("Кубик №",x," :",point)
+            print("Кубик №",x + 1," :",point)
             sum_point += point
         print("Сумма очков всех кубиков :",sum_point,"\n")
-
 
 print("Game Over!")
