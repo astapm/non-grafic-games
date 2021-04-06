9f7c6fbf5a66c22ac7ff6dee2195e98166efc65f
Mikhail Astapchyk <74508872+astapm@users.noreply.github.com>
2021-04-06 22:35:35 +0300

    Update f1.py

--- dice-racing/f1.py
+++ dice-racing/f1.py
@@ -28,7 +28,7 @@ if num_cars.isdigit():
 else:
     num_cars = 2
 
-# Параметры трассы
+## Параметры трассы
 # Количество клеток трассы
 track_cells = 100
 
@@ -44,36 +44,64 @@ places = 1
 # Флаг пересечения финиша 
 flag_places = 0
 
-# Число секунд паузы между ходами
+# Число секунд паузы между ходами для оценки ситуации на трассе
 sleep = 4
 
 # Список названий команд и их трёхбуквенных сокращений
-names_teams = ["Мерседес", "Феррари", "Рено", "Ред Булл", "МакЛарен", "Маруся"]
-short_names_teams = ["Мер", "Фер", "Рен", "Ред", "Мак", "Мар"]
+names_teams = "Мерседес", "Феррари", "Рено", "Ред Булл", "МакЛарен", "Маруся"
+short_names_teams = "МРСc", "ФЕР", "РЕН", "РБЛ", "МКЛ", "МАР"
 
-# Создаём таблицу заезда
+## Создаём таблицу заезда
+
+# Номер машины
+# Команда
+# Краткое имя команды
+# Текущие шины
+# Деградация шин
+# Текущая клетка
+# Финишная клетка
+# Текущая передача
+# Финишная передача
+# Статус машины в гонке
+
 # Список номеров машин 
 numers = list(range(1, num_cars + 1))
 # numers = list(n for n in range(num_cars))
+
+# Список команд
 teams = names_teams[:num_cars]
 # teams = [names_teams[x] for x in range(num_cars)]
 short_names = short_names_teams[:num_cars]
+
+# Текущие шины
 tires = [None]*num_cars
 # tires = [None for _ in range(num_cars)]
-cell = [0]*num_cars
-end_cell = [None]*num_cars
-transmission = [None]*num_cars
-end_transmission = [None]*num_cars
-status = [" "]*num_cars
-tires_degradation = [None]*num_cars
-end_rolls = [None]*num_cars
 
-# Создаём шаблон полей для вывода в табличном виде номеров ячеек и инфо-строки
+# Текущая клетка
+cell = [0]*num_cars
+
+# Финишная клетка
+end_cell = [None]*num_cars
+
+# Текущая передача/скорость
+transmission = [None]*num_cars
+
+# Передача/скорость на финише
+end_transmission = [None]*num_cars
+
+# Деградация шин
+tires_degradation = [None]*num_cars
+
+# Статус машины в гонке
+status = [" "]*num_cars
+
+## Создаём шаблон полей для вывода в табличном виде номеров ячеек и инфо-строки
 out_table = "{:>3}"+"{:>4}"*(num_cars - 1)
 
-# Начало игры
+## Начало игры
 flag_racing = 1 # флаг гонки вверху
 
+## Функция броска игральной кости и передвижение по клеткам
 def roll(x):
     return x + randint(1,6)
 
@@ -85,8 +113,9 @@ print("СТАРТ!".center(4*num_cars))
 while " " in status:
     # пауза чтобы оценить ситуацию в гонке
     time.sleep(4)
+    # Бросаем кубик и передвигаем машинку
     for x in range(num_cars):
-        if cell[x] < 100:
+        if cell[x] < track_cells:
             cell[x] = cell[x] + randint(1,6)
         else:
             status[x] = "F"
@@ -94,4 +123,3 @@ while " " in status:
     # cell = [points + randint(1,6) for points in cell if int(points) < 100]
     print(out_table.format(*cell))  # вывод информации о позиции на трассе
     print(out_table.format(*status)) # вывод информации о местах на финише
-
